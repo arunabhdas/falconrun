2f4d5635cc3c834b73906193e9ca886dd904cead
Arunabh Das <arunabhdas@gmail.com>
2017-01-12 02:57:24 -0500

    Added tkinter dialog1

---
+++ dialog1.py
@@ -0,0 +1,31 @@
+try:
+    # for Python2
+    from Tkinter import *   ## notice capitalized T in Tkinter 
+except ImportError:
+    # for Python3
+    from tkinter import *   ## notice lowercase 't' in tkinter here
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print ("hi there, everyone!")
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
+root.destroy() # optional; see description below
