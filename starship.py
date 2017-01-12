1e170820d70e02fdf3f3d8429ba59822d16aee1d
Arunabh Das <arunabhdas@gmail.com>
2017-01-12 01:50:54 -0500

    Improvements to Starship using dict

--- starship.py
+++ starship.py
@@ -1,8 +1,10 @@
 class Starship:
-	energy = 1
-	wavelength = 'blue'
-	payload = 'lightshield'
-	engine = 'warpdrive'
+
+	def __init__(self, **kwargs):
+		self.energy = kwargs.get('energy', 1)
+		self.wavelength = kwargs.get('wavelength', 'blue')
+		self.payload = kwargs.get('payload', 'lasers')
+		self.engine = kwargs.get('engine', 'warpdrive')
 
 	def engage(self):
 		return self.engine.upper()
