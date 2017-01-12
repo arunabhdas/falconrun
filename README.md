8b880920c5c83c1cee3feda148e593c6af308eca
Arunabh Das <arunabhdas@gmail.com>
2017-01-12 01:31:43 -0500

    Added engage method Cleanup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # falconrun
+
+gitignore sourced from from here :
+
+https://github.com/github/gitignore/blob/master/Python.gitignore
--- starship.py
+++ starship.py
@@ -2,3 +2,8 @@ class Starship:
 	energy = 1
 	wavelength = 'blue'
 	payload = 'lightshield'
+	engine = 'warpdrive'
+
+	def engage(self):
+		return self.engine.upper()
+
